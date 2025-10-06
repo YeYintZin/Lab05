f308613146f7712376806ffba7f525314e9e9a63
Ye Yint Zin <6289490@edu.vaniercollege.qc.ca>
2025-10-06 08:38:53 -0400

    start

---
+++ src/lab05/Lab05.java
@@ -0,0 +1,16 @@
+package lab05;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Lab05 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
